bad0c83ef0502a8ad72a95af567c9b85792e205a
Amiran <chicobes1@gmail.com>
2022-02-05 21:46:52 +0300

    complete

---
+++ Custom Video Player/scripts.js
@@ -0,0 +1,55 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.full-screen');
+function togglePlay()
+{
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+function updateButton()
+{
+    const icon = this.paused ? '►' : '||';
+    toggle.textContent = icon;
+}
+function skip()
+{
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function heandleRangeUpdate()
+{
+    video[this.name] = this.value
+}
+function hendleprogress()
+{
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(event)
+{
+    const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+function fullScreenActive()
+{
+    video.requestFullscreen();
+}
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', hendleprogress);
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', heandleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', heandleRangeUpdate));
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', ()=> mousedown && scrub(event));
+progress.addEventListener('mousedown', ()=> mousedown = true);
+progress.addEventListener('mouseup', ()=> mousedown = false);
+fullScreen.addEventListener('click', fullScreenActive);
